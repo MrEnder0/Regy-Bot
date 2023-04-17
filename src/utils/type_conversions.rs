6d0e6e5bc3f59678e82a0458c3eb1abb152a9b47
MrEnder0 <endermobtrey@gmail.com>
2023-04-17 17:43:12 -0500

    simplified function

--- src/utils/type_conversions.rs
+++ src/utils/type_conversions.rs
@@ -5,9 +5,5 @@ pub fn string_to_static_str(s: String) -> &'static str {
 }
 
 pub fn userid_to_u64(userid: UserId) -> u64 {
-    let userid = userid.to_string();
-    let userid = userid.replace("<@!", "");
-    let userid = userid.replace('>', "");
-    let userid = userid.parse::<u64>().unwrap();
-    userid
+    return userid.to_string().replace("<@!", "").replace('>', "").parse::<u64>().unwrap();
 }
