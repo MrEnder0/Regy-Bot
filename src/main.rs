b5054e290b7c7ea1b7539a41fc45447382a3c52d
MrEnder0 <endermobtrey@gmail.com>
2023-04-30 21:13:34 -0500

    automod intergration

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod utils;
 
 use poise::{
     Event,
-    serenity_prelude as serenity,
+    serenity_prelude::{self as serenity},
     serenity_prelude::{
         CreateEmbed,
         ReactionType,
@@ -17,7 +17,7 @@ use std::{
 };
 use regex::Regex;
 
-use crate::utils::{toml::*, logger::*};
+use crate::utils::{toml::*, logger::*, type_conversions::*};
 use crate::commands::{user::*, staff::*, dev::*};
 
 pub struct Data {}
@@ -303,6 +303,34 @@ async fn main() {
                                     return Ok(());
                                 }
                             }
+                        }
+                        Event::AutoModerationActionExecution { execution } => {
+                            //IF action is BlockMessage
+                            if execution.action != serenity::model::guild::automod::Action::BlockMessage {
+                                return Ok(());
+                            }
+
+                            let user = execution.user_id;
+                            let message = execution.matched_content.clone().unwrap();
+
+                            let dm_msg = format!("You are not allowed to send messages with blocked content which breaks the server's setup regex rules, this has been reported to the server staff, continued infractions will result in greater punishment.\n\n\
+                                                    The message which has been blocked is below:\n\
+                                                    ||{}||", message);
+
+                            let user = UserId(userid_to_u64(user));
+                            let user = user.to_user(&ctx.http).await.expect("Unable to get user");
+                            user.dm(&ctx.http, |m| m.content(dm_msg)).await.expect("Unable to dm user");
+                            let log_channel = ChannelId(get_config().log_channel);
+
+                            let mut embed = CreateEmbed::default();
+                            embed.color(0xFFA500);
+                            embed.title("Message blocked due to matching a set automod pattern");
+                            embed.field("The user who broke a automod pattern is below:", format!("{}", user), false);
+                            embed.field("Their message is the following below:", format!("||{}||", message), false);
+                            embed.footer(|f| f.text("React with 🚫 to dismiss this infraction"));
+                            let embed_message_id = log_channel.send_message(&ctx.http, |m| m.set_embed(embed)).await.expect("Unable to send embed").id;
+                            let embed_message = log_channel.message(&ctx.http, embed_message_id).await.ok();
+                            embed_message.unwrap().react(&ctx.http, ReactionType::Unicode("🚫".to_string())).await.ok();
                         }
                         Event::GuildBanAddition { guild_id: _, banned_user } => {
                             if get_config().user_delete_on_ban {
