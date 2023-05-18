7c31dc3f05723986ee37144389bff0725466e25b
MrEnder0 <endermobtrey@gmail.com>
2023-05-18 16:34:24 -0500

    update helper prints

--- src/update_helper.rs
+++ src/update_helper.rs
@@ -11,20 +11,21 @@ fn main() {
     let regy_bin = "regy_bot.exe";
 
     if !Path::new(regy_bin).exists() {
-        println!("Regy binary does not exist, shutting down.");
+        println!("[Update Helper ERROR] Regy binary does not exist, shutting down.");
         return;
     }
 
     if !Path::new("updated").exists() {
-        println!("Regy has not been updated, shutting down.");
+        println!("[Update Helper WARNING] Regy has not been updated, shutting down.");
         return;
     }
 
-    println!("Regy has finished updating restarting Regy.");
+    println!("[Update Helper INFO] Regy has finished updating restarting Regy.");
     std::fs::remove_file("updated").expect("Failed to remove updated file");
     std::thread::spawn(|| {
         Command::new("regy_bot.exe").spawn().expect("Failed to start regy_bot.exe");
     });
 
+    println!("[Update Helper INFO] Update helper has finished, closing update helper.");
     std::process::exit(0);
 }
