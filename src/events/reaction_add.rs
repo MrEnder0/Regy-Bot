498adb40865d1256fd40e4cf374be516ec3d558d
MrEnder0 <endermobtrey@gmail.com>
2023-05-19 16:32:45 -0500

    fixed logic for dismis

--- src/events/reaction_add.rs
+++ src/events/reaction_add.rs
@@ -9,6 +9,7 @@ use poise::{
 use crate::utils::{toml::*, logger::*, log_on_error::LogExpect};
 
 pub async fn reaction_add_event(ctx: &serenity::Context, add_reaction: &serenity::Reaction) {
+    println!("0");
     //ignore reactions from the bot
     if add_reaction.user_id.unwrap() == ctx.cache.current_user_id() {
         return;
@@ -20,7 +21,7 @@ pub async fn reaction_add_event(ctx: &serenity::Context, add_reaction: &serenity
     }
 
     //ignore events except for staff
-    if !get_config().moderators.contains(&add_reaction.user_id.unwrap().to_string()) || !get_config().admins.contains(&add_reaction.user_id.unwrap().to_string()) {
+    if !get_config().admins.contains(&add_reaction.user_id.unwrap().to_string()) && !get_config().moderators.contains(&add_reaction.user_id.unwrap().to_string()) {
         return;
     }
 
@@ -28,6 +29,8 @@ pub async fn reaction_add_event(ctx: &serenity::Context, add_reaction: &serenity
     if add_reaction.emoji != ReactionType::Unicode("🚫".to_string()) {
         return;
     }
+
+    println!("1");
 
     let ctx_clone = ctx.clone();
     let reaction_clone = add_reaction.clone();
