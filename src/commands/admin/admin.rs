d19489319a55295870149fa5850115e43bb269e0
MrEnder0 <endermobtrey@gmail.com>
2023-05-29 23:18:16 -0500

    port regex commands

--- src/commands/admin/admin.rs
+++ src/commands/admin/admin.rs
@@ -30,34 +30,6 @@ pub async fn admin(
             ctx.say("You need to specify a command.").await?;
             Ok(())
         }
-        "add_regex" => {
-            let args = arg.split_whitespace().skip(1).collect::<Vec<&str>>();
-            println!("{:?}", args);
-            let mut new_block_phrase = String::new();
-            for arg in args {
-                new_block_phrase.push_str(arg);
-                new_block_phrase.push(' ');
-            }
-
-            //Prevents for empty regex
-            if new_block_phrase.is_empty() || new_block_phrase == " " || new_block_phrase.len() < 3
-            {
-                ctx.say("You need to specify a regex phrase to add; it cant be empty and it also cant be less than 3 characters long.").await.log_expect("Unable to send message");
-                return Ok(());
-            }
-
-            let new_block_phrase_clone = new_block_phrase.clone();
-            toml::add_block_phrase(new_block_phrase);
-
-            let status_message = format!(
-                "Added the regex phrase:\n||```{}```||",
-                new_block_phrase_clone
-            );
-            ctx.say(status_message)
-                .await
-                .log_expect("Unable to send message");
-            Ok(())
-        }
         "remove_regex" => {
             let id = arg.split_whitespace().nth(1).unwrap_or("none");
             if id == "none" {
@@ -72,59 +44,6 @@ pub async fn admin(
             ctx.say(status_message)
                 .await
                 .log_expect("Unable to send message");
-            Ok(())
-        }
-        "list_regex" => {
-            let status_msg = ctx
-                .say("Sending regex phrases this may take a few seconds...")
-                .await
-                .log_expect("Unable to send message");
-            let blocked_phrases = toml::list_block_phrases();
-            let mut formatted_blocked_phrases = String::new();
-            for (id, phrase) in blocked_phrases {
-                formatted_blocked_phrases.push_str(&id.to_string());
-                formatted_blocked_phrases.push_str(" | ");
-                formatted_blocked_phrases.push_str(&phrase);
-                formatted_blocked_phrases.push('\n');
-            }
-
-            let status_message = format!("The current regex being used are **[WARNING CONTAINS SENSITIVE MESSAGES]**\n||```                  ID                 | REGEX\n{}```||", formatted_blocked_phrases);
-            let channel_id = ctx.channel_id();
-
-            if status_message.len() > 2000 {
-                channel_id.say(ctx, "The current regex being used are **[WARNING CONTAINS SENSITIVE MESSAGES]**").await?;
-                let mut split_status_message = String::new();
-                //remove the warning message
-                let status_message = status_message[75..status_message.len()].to_string();
-                let status_message = status_message[5..status_message.len() - 5].to_string();
-                let mut line_count = 0;
-                for line in status_message.lines() {
-                    split_status_message.push_str(line);
-                    split_status_message.push('\n');
-                    line_count += 1;
-                    if line_count == 5 {
-                        let message_part = format!("```{}```", split_status_message);
-                        channel_id
-                            .say(ctx, message_part)
-                            .await
-                            .log_expect("Unable to send message");
-                        split_status_message = String::new();
-                        line_count = 0;
-                        tokio::time::sleep(std::time::Duration::from_millis(40)).await;
-                    }
-                }
-            } else {
-                ctx.say(status_message)
-                    .await
-                    .log_expect("Unable to send message");
-            }
-
-            status_msg
-                .edit(ctx, |m| {
-                    m.content("Finished sending regex phrases to the channel.")
-                })
-                .await
-                .log_expect("Unable to edit message");
             Ok(())
         }
         _ => {
---
+++ src/commands/regex/remove_regex.rs
@@ -0,0 +1,30 @@
+use uuid::Uuid;
+
+use crate::{
+    utils::logger::LogExpect,
+    utils::toml,
+    Data,
+};
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+#[poise::command(prefix_command, slash_command, user_cooldown = 10, required_permissions = "ADMINISTRATOR")]
+pub async fn remove_regex(
+    ctx: Context<'_>,
+    #[description = "Regex id"] id: String
+) -> Result<(), Error> {
+    if id == "none" {
+        ctx.say("You need to specify a target UUID.")
+            .await
+            .log_expect("Unable to send message");
+        return Ok(());
+    }
+    let id = id.parse::<Uuid>().unwrap();
+    toml::remove_block_phrase(id);
+    let status_message = format!("Removed the regex phrase with UUID: {}", id);
+    ctx.say(status_message)
+        .await
+        .log_expect("Unable to send message");
+    Ok(())
+}
