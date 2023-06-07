acd32129c60ace8937a46ea358887d2b9e61579e
MrEnder0 <endermobtrey@gmail.com>
2023-06-06 23:53:32 -0500

    update helper color logging

--- src/update_helper.rs
+++ src/update_helper.rs
@@ -15,7 +15,6 @@ fn main() {
         message: "[Update Helper] Waiting for Regy to fully shutdown.".to_string(),
     };
     log_this(data);
-    println!("[Update Helper INFO] Waiting for Regy to fully shutdown.");
     std::thread::sleep(Duration::from_secs(3));
 
     let regy_bin = "regy_bot.exe";
@@ -23,10 +22,9 @@ fn main() {
     if !Path::new(regy_bin).exists() {
         let data = LogData {
             importance: LogImportance::Error,
-            message: "[Update Helper] Regy binary does not exist, shutting down.".to_string(),
+            message: "[Update Helper] Updated Regy binary does not exist, shutting down.".to_string(),
         };
         log_this(data);
-        println!("[Update Helper ERROR] Regy binary does not exist, shutting down.");
         return;
     }
 
@@ -36,20 +34,17 @@ fn main() {
             message: "[Update Helper] Regy is not in update state, shutting down.".to_string(),
         };
         log_this(data);
-        println!("[Update Helper WARNING] Regy is not in update state, shutting down.");
         return;
     }
 
-    println!("[Update Helper INFO] Regy has finished updating restarting Regy.");
     let data = LogData {
         importance: LogImportance::Info,
         message: "[Update Helper] Regy has finished updating restarting Regy.".to_string(),
     };
     log_this(data);
-    std::fs::remove_file("updated").log_expect("Failed to remove updated file");
-    Command::new("regy_bot.exe").spawn().log_expect("Failed to start regy_bot.exe");
+    std::fs::remove_file("updated").log_expect(LogImportance::Error, "Failed to remove updated file");
+    Command::new("regy_bot.exe").spawn().log_expect(LogImportance::Error, "Failed to start regy_bot.exe");
 
-    println!("[Update Helper INFO] Update helper has finished, closing update helper.");
     let data = LogData {
         importance: LogImportance::Info,
         message: "[Update Helper] Update helper has finished, closing update helper.".to_string(),
