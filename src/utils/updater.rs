4ec08d7479ff968b1bce388f65924411e6549876
MrEnder0 <endermobtrey@gmail.com>
2023-06-07 00:34:48 -0500

    changed error type to appropriate level

--- src/utils/updater.rs
+++ src/utils/updater.rs
@@ -12,9 +12,9 @@ pub fn local_update(new_bin: &str) -> i8 {
         return -1;
     }
 
-    self_replace::self_replace(new_bin).log_expect(LogImportance::Warning, "Failed to update binary");
-    std::fs::remove_file(new_bin).log_expect(LogImportance::Warning, "Failed to remove temp binary");
-    std::fs::File::create("updated").log_expect(LogImportance::Warning, "Failed to create temp file");
+    self_replace::self_replace(new_bin).log_expect(LogImportance::Error, "Failed to update binary");
+    std::fs::remove_file(new_bin).log_expect(LogImportance::Error, "Failed to remove temp binary");
+    std::fs::File::create("updated").log_expect(LogImportance::Error, "Failed to create temp file");
 
     if !Path::new("regy_bot_update_helper.exe").exists() {
         return 1;
