890638e08011246c5c4f34ae6c9c15e3c3bc2c8a
MrEnder0 <endermobtrey@gmail.com>
2023-06-06 23:37:48 -0500

    color logging support for updater

--- src/utils/updater.rs
+++ src/utils/updater.rs
@@ -1,7 +1,7 @@
 use self_replace;
 use std::path::Path;
 
-use crate::utils::logger::LogExpect;
+use crate::utils::logger::{LogImportance, LogExpect};
 
 pub fn local_update(new_bin: &str) -> i8 {
     if !Path::new(new_bin).exists() {
@@ -12,9 +12,9 @@ pub fn local_update(new_bin: &str) -> i8 {
         return -1;
     }
 
-    self_replace::self_replace(new_bin).log_expect("Failed to update binary");
-    std::fs::remove_file(new_bin).log_expect("Failed to remove temp binary");
-    std::fs::File::create("updated").log_expect("Failed to create temp file");
+    self_replace::self_replace(new_bin).log_expect(LogImportance::Warning, "Failed to update binary");
+    std::fs::remove_file(new_bin).log_expect(LogImportance::Warning, "Failed to remove temp binary");
+    std::fs::File::create("updated").log_expect(LogImportance::Warning, "Failed to create temp file");
 
     if !Path::new("regy_bot_update_helper.exe").exists() {
         return 1;
