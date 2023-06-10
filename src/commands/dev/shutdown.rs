80226da0c05918b49e87e6dc825ef9421ca4176a
MrEnder0 <endermobtrey@gmail.com>
2023-06-10 17:59:31 -0500

    changed shutdown command

--- src/commands/dev/shutdown.rs
+++ src/commands/dev/shutdown.rs
@@ -33,39 +33,10 @@ pub async fn shutdown(
         message: format!("Shutdown from dev commands sent from {}", msg_author),
     });
 
-    ctx.say("Initialized shutdown countdown for 90 seconds")
+    ctx.say("Shutting down...")
         .await
         .log_expect(LogImportance::Warning, "Unable to send message");
 
-    for i in 0..90 {
-        let mut embed = CreateEmbed::default();
-        embed.color(0x565e6e);
-        embed.title("Regy Shutdown");
-        if i > 80 {
-            embed.description(format!(
-                ":warning: Regy will be shutdown in the following seconds: {}",
-                90 - i
-            ));
-        } else {
-            embed.description(format!(
-                "Regy will be shutdown in the following seconds: {}",
-                90 - i
-            ));
-        }
-        embed.thumbnail("https://raw.githubusercontent.com/MrEnder0/Regy-Bot/master/.github/assets/shutdown.png");
-        embed.footer(|f| f.text("This force shutdown was sent from a dev"));
-        let embed_message = ctx
-            .channel_id()
-            .send_message(&ctx, |m| m.set_embed(embed))
-            .await
-            .log_expect(LogImportance::Warning, "Unable to send shutdown embed")
-            .id;
-        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
-        ctx.channel_id()
-            .delete_message(&ctx, embed_message)
-            .await
-            .ok();
-    }
-    ctx.say("Countdown finished, shutting down...").await?;
+    ctx.framework().shard_manager().lock().await.shutdown_all().await;
     std::process::exit(0);
 }
