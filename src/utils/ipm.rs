bf95a2e965ec568e889f7fccd7c5b70653146661
MrEnder0 <endermobtrey@gmail.com>
2023-08-01 02:26:41 -0500

    ipm fixes

--- src/utils/ipm.rs
+++ src/utils/ipm.rs
@@ -1,27 +1,28 @@
-use once_cell::sync::Lazy;
-use std::{collections::HashMap, sync::Mutex};
+use std::{collections::BTreeMap, sync::Mutex};
 
 use crate::utils::toml::read_config;
 
-const IPM: Lazy<Mutex<HashMap<u64, u16>>> = Lazy::new(|| Mutex::new(HashMap::new()));
+static IPM: Mutex<BTreeMap<u64, u16>> = Mutex::new(BTreeMap::new());
 
 pub struct IpmStruct {}
 
 impl IpmStruct {
     pub fn get_server(server_id: u64) -> u16 {
-        let binding = IPM;
+        let binding = &IPM;
         let guard = binding.lock().unwrap();
-
-        guard[&server_id]
+        match guard.get(&server_id) {
+            Some(value) => *value,
+            None => 0,
+        }
     }
     pub fn set_server(server_id: u64, value: u16) {
-        let binding = IPM;
+        let binding = &IPM;
         let mut guard = binding.lock().unwrap();
         guard.insert(server_id, value);
     }
     pub fn get_overflow() -> Vec<u64> {
         let mut overflow: Vec<u64> = Vec::new();
-        let binding = IPM;
+        let binding = &IPM;
         let guard = binding.lock().unwrap();
         for (key, value) in guard.iter() {
             if value > &read_config().global.max_activity_influx {
@@ -32,7 +33,7 @@ impl IpmStruct {
         overflow
     }
     pub fn increment_server(server_id: u64) {
-        let binding = IPM;
+        let binding = &IPM;
         let mut guard = binding.lock().unwrap();
         if !guard.contains_key(&server_id) {
             guard.insert(server_id, 1);
