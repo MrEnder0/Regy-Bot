b634de1484c25a13d520062fb1096d04b22480cb
MrEnder0 <72358826+MrEnder0@users.noreply.github.com>
2023-08-09 20:05:34 -0500

    fmt

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,6 +1,6 @@
 pub mod ipm;
-pub mod rti;
 pub mod perm_check;
+pub mod rti;
 pub mod toml;
 pub mod type_conversions;
 pub mod updater;
--- src/utils/rti.rs
+++ src/utils/rti.rs
@@ -8,7 +8,7 @@ impl RtiStruct {
     pub fn load() {
         let binding = &RTI;
         let mut guard = binding.lock().unwrap();
-        
+
         //TODO: Fetch rti (regex template index) file from github and parse it into the BTreeMap
     }
 
