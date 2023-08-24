037a61170f9a5a002e981cfd813e517aa3cd5bec
MrEnder0 <72358826+MrEnder0@users.noreply.github.com>
2023-08-24 17:18:36 -0500

    add clone to rti object

--- src/utils/rti.rs
+++ src/utils/rti.rs
@@ -4,14 +4,14 @@ use reqwest::blocking::get;
 use ron::{self, de::from_reader};
 use scorched::{log_this, LogData, LogExpect, LogImportance};
 use serde::{Deserialize, Serialize};
-use std::{error::Error, fs::File};
+use std::fs::File;
 
 #[derive(Serialize, Deserialize)]
 pub struct MetaData {
     pub version: u32,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct RtiObject {
     pub uuid: String,
     pub phrase: String,
@@ -31,17 +31,6 @@ pub async fn download_rti() {
 
     let mut file = File::create("rti_packages.ron").unwrap();
     response.copy_to(&mut file).unwrap();
-}
-
-pub fn load_rti() -> Result<Vec<RtiObject>, Box<dyn Error>> {
-    let mut return_vec = Vec::new();
-    let rti: Vec<RtiObject> = from_reader(File::open("rti_packages.ron")?)?;
-
-    for rti_object in rti {
-        return_vec.push(rti_object);
-    }
-
-    Ok(return_vec)
 }
 
 fn read_rti() -> RtiPackages {
