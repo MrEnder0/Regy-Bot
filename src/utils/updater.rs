2aeb0be4436f051a925aecb1c5d50339616de8c6
MrEnder0 <72358826+MrEnder0@users.noreply.github.com>
2023-08-31 16:06:40 -0500

    lock changes

--- src/utils/updater.rs
+++ src/utils/updater.rs
@@ -11,12 +11,14 @@ pub fn local_update(new_bin: &str) -> i8 {
         return -1;
     }
 
-    std::fs::File::create("update.lock").log_expect(LogImportance::Error, "Failed to create temp file");
+    std::fs::File::create("update.lock")
+        .log_expect(LogImportance::Error, "Failed to create temp file");
     self_replace::self_replace(new_bin).log_expect(LogImportance::Error, "Failed to update binary");
     std::fs::remove_file(new_bin).log_expect(LogImportance::Error, "Failed to remove temp binary");
 
     if !Path::new("regy_bot_update_helper.exe").exists() {
-        std::fs::remove_file("update.lock").log_expect(LogImportance::Error, "Failed to remove update.lock file");
+        std::fs::remove_file("update.lock")
+            .log_expect(LogImportance::Error, "Failed to remove update.lock file");
         return 1;
     }
 
