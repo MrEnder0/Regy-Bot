700ed437a7b88791c3b1bc8bfb2fd4d7fb8d6fa4
MrEnder0 <72358826+MrEnder0@users.noreply.github.com>
2023-08-30 20:44:25 -0500

    update lock

--- src/update_helper.rs
+++ src/update_helper.rs
@@ -27,7 +27,7 @@ async fn main() {
         return;
     }
 
-    if !Path::new("updated").exists() {
+    if !Path::new("update.lock").exists() {
         log_this(LogData {
             importance: LogImportance::Warning,
             message: "[Update Helper] Regy is not in update state, shutting down.".to_string(),
@@ -43,8 +43,8 @@ async fn main() {
     })
     .await;
 
-    std::fs::remove_file("updated")
-        .log_expect(LogImportance::Error, "Failed to remove updated file");
+    std::fs::remove_file("update.lock")
+        .log_expect(LogImportance::Error, "Failed to remove update.lock file");
     Command::new("regy_bot.exe")
         .spawn()
         .log_expect(LogImportance::Error, "Failed to start regy_bot.exe");
--- src/utils/updater.rs
+++ src/utils/updater.rs
@@ -7,15 +7,16 @@ pub fn local_update(new_bin: &str) -> i8 {
         return 0;
     }
 
-    if Path::new("updated").exists() {
+    if Path::new("update.lock").exists() {
         return -1;
     }
 
+    std::fs::File::create("update.lock").log_expect(LogImportance::Error, "Failed to create temp file");
     self_replace::self_replace(new_bin).log_expect(LogImportance::Error, "Failed to update binary");
     std::fs::remove_file(new_bin).log_expect(LogImportance::Error, "Failed to remove temp binary");
-    std::fs::File::create("updated").log_expect(LogImportance::Error, "Failed to create temp file");
 
     if !Path::new("regy_bot_update_helper.exe").exists() {
+        std::fs::remove_file("update.lock").log_expect(LogImportance::Error, "Failed to remove update.lock file");
         return 1;
     }
 
