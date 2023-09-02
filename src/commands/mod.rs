0cfa3ebd9aee82f3ad56577a30676519760c84ee
MrEnder0 <72358826+MrEnder0@users.noreply.github.com>
2023-09-01 22:37:13 -0500

    non main.rs command linking

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,14 +1,52 @@
 pub mod dev;
 
-//User
+// User
 pub mod general;
 pub mod info;
 
-//Staff
+// Staff
 pub mod infractions;
 pub mod moderation;
 pub mod rti;
 
-//Administrator
+// Administrator
 pub mod management;
 pub mod regex;
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
+
+// Link all commands into vec
+pub fn commands() -> Vec<poise::Command<super::Data, Error>> {
+    vec![
+        general::help::help(),
+        general::permission_level::permission_level(),
+        info::about::about(),
+        info::why_rust::why_rust(),
+        info::what_is_regex::what_is_regex(),
+        info::skid::skid(),
+        infractions::my_infractions::my_infractions(),
+        infractions::add_infraction::add_infraction(),
+        infractions::dismiss_infraction::dismiss_infraction(),
+        infractions::list_infractions::list_infractions(),
+        moderation::grab_pfp::grab_pfp(),
+        moderation::nuke::nuke(),
+        regex::add_regex::add_regex(),
+        regex::remove_regex::remove_regex(),
+        regex::list_regex::list_regex(),
+        management::add_staff::add_staff(),
+        management::remove_staff::remove_staff(),
+        management::list_staff::list_staff(),
+        management::config_setup::config_setup(),
+        management::config_clone::config_clone_regex(),
+        dev::upload_logs::upload_logs(),
+        dev::clean_logs::clean_logs(),
+        dev::get_ipm::get_ipm(),
+        dev::reset_ipm::reset_ipm(),
+        dev::echo::echo(),
+        dev::shutdown::shutdown(),
+        dev::local_update::update(),
+        rti::search_rti::search_rti(),
+        rti::update_rti::update_rti(),
+        rti::reload_rti::reload_rti(),
+    ]
+}
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ mod utils;
 use poise::{serenity_prelude as serenity, Event};
 use std::path::Path;
 
-use crate::commands::*;
 use crate::events::*;
 use crate::utils::config::*;
 use crate::utils::ipm::*;
@@ -25,38 +24,7 @@ async fn main() {
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![
-                general::help::help(),
-                general::permission_level::permission_level(),
-                info::about::about(),
-                info::why_rust::why_rust(),
-                info::what_is_regex::what_is_regex(),
-                info::skid::skid(),
-                infractions::my_infractions::my_infractions(),
-                infractions::add_infraction::add_infraction(),
-                infractions::dismiss_infraction::dismiss_infraction(),
-                infractions::list_infractions::list_infractions(),
-                moderation::grab_pfp::grab_pfp(),
-                moderation::nuke::nuke(),
-                regex::add_regex::add_regex(),
-                regex::remove_regex::remove_regex(),
-                regex::list_regex::list_regex(),
-                management::add_staff::add_staff(),
-                management::remove_staff::remove_staff(),
-                management::list_staff::list_staff(),
-                management::config_setup::config_setup(),
-                management::config_clone::config_clone_regex(),
-                dev::upload_logs::upload_logs(),
-                dev::clean_logs::clean_logs(),
-                dev::get_ipm::get_ipm(),
-                dev::reset_ipm::reset_ipm(),
-                dev::echo::echo(),
-                dev::shutdown::shutdown(),
-                dev::local_update::update(),
-                rti::search_rti::search_rti(),
-                rti::update_rti::update_rti(),
-                rti::reload_rti::reload_rti(),
-            ],
+            commands: commands::commands(),
             event_handler: |ctx, event, _framework, _data| {
                 Box::pin(async move {
                     match event {
