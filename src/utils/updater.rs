c174788c3e4d5378c505474c493521d07dc5d184
MrEnder0 <72358826+MrEnder0@users.noreply.github.com>
2023-11-10 13:23:10 -0600

    Update Regy binary version and improve update-helper shutdown process

--- src/utils/updater.rs
+++ src/utils/updater.rs
@@ -7,20 +7,21 @@ pub fn local_update(new_bin: &str) -> i8 {
         return 0;
     }
 
+    if !Path::new("regy_bot_update_helper.exe").exists() {
+        std::fs::remove_file("update.lock")
+            .log_expect(LogImportance::Info, "No update.lock file found");
+        return 1;
+    }
+
     if Path::new("update.lock").exists() {
         return -1;
     }
 
     std::fs::File::create("update.lock")
-        .log_expect(LogImportance::Error, "Failed to create temp file");
+        .log_expect(LogImportance::Error, "Failed to create lock file file");
+
     self_replace::self_replace(new_bin).log_expect(LogImportance::Error, "Failed to update binary");
     std::fs::remove_file(new_bin).log_expect(LogImportance::Error, "Failed to remove temp binary");
-
-    if !Path::new("regy_bot_update_helper.exe").exists() {
-        std::fs::remove_file("update.lock")
-            .log_expect(LogImportance::Error, "Failed to remove update.lock file");
-        return 1;
-    }
 
     2
 }
