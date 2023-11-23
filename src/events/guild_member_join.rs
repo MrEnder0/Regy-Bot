041c2a4629124d727ecd83ead1c1cc143f2e164c
MrEnder0 <72358826+MrEnder0@users.noreply.github.com>
2023-11-22 22:45:56 -0600

    kick logging

--- src/events/guild_member_join.rs
+++ src/events/guild_member_join.rs
@@ -1,5 +1,5 @@
 use ::regex::Regex;
-use poise::{serenity_prelude as serenity, serenity_prelude::Member};
+use poise::{serenity_prelude as serenity, serenity_prelude::{Member, ChannelId}};
 use scorched::{LogExpect, LogImportance};
 
 use crate::utils::{
@@ -36,7 +36,7 @@ pub async fn guild_member_join_event(ctx: &serenity::Context, guild_member: &Mem
             .dm(&ctx.http, |m| {
                 m.embed(|e| {
                     e.title("Username contains blocked phrase");
-                    e.description("Your username or nickname contains a banned word, please change it. You will be given 2 minutes to change it, if you do not change it you will be kicked from the server, you will be able to rejoin if you do get kicked when you fix your username or nickname. If you do not know what you did wrong, please contact a staff member. Your username or nickname")
+                    e.description("Your username or nickname contains a banned word, please change it. You will be given 1 and a half minutes to change it, if you do not change it you will be kicked from the server, you will be able to rejoin if you do get kicked when you fix your username or nickname. If you do not know what you did wrong, please contact a staff member. Your username or nickname")
                 })
             })
             .await
@@ -46,7 +46,7 @@ pub async fn guild_member_join_event(ctx: &serenity::Context, guild_member: &Mem
         let guild_member_clone = guild_member.clone();
 
         tokio::spawn(async move {
-            tokio::time::sleep(tokio::time::Duration::from_secs(120)).await;
+            tokio::time::sleep(tokio::time::Duration::from_secs(90)).await;
 
             // Kicks user if they have not changed their username or nickname
             if list_regex(guild_member_clone.guild_id.to_string())
@@ -63,6 +63,29 @@ pub async fn guild_member_join_event(ctx: &serenity::Context, guild_member: &Mem
                     .kick(&ctx_clone.http.clone())
                     .await
                     .log_expect(LogImportance::Error, "Failed to kick user");
+
+                // Send message to log channel
+                let log_channel = ChannelId(
+                    read_config()
+                        .await
+                        .servers
+                        .get(&guild_member_clone.guild_id.to_string())
+                        .unwrap()
+                        .log_channel,
+                );
+                
+                log_channel
+                    .send_message(&ctx_clone.http, |m| {
+                        m.embed(|e| {
+                            e.title("User kicked for an offensive profile");
+                            e.description(format!(
+                                "{} was kicked for not changing their username or nickname, they are able to rejoin if they change their username or nickname.",
+                                guild_member_clone.user.name
+                            ))
+                        })
+                    })
+                    .await
+                    .log_expect(LogImportance::Error, "Failed to send message to log channel");
             }
         });
     }
