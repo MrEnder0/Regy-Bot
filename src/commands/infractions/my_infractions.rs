241c892716b45203de633082e7113ac965475cea
MrEnder0 <72358826+MrEnder0@users.noreply.github.com>
2023-12-01 17:32:50 -0600

    Better reply for no infractions

--- src/commands/infractions/my_infractions.rs
+++ src/commands/infractions/my_infractions.rs
@@ -2,7 +2,7 @@ use scorched::*;
 
 use crate::{
     utils::{
-        config::{self, infractions},
+        config::{self, infractions, structs::UserOffenses},
         type_conversions::userid_to_u64,
     },
     Data,
@@ -22,7 +22,7 @@ pub async fn my_infractions(ctx: Context<'_>) -> Result<(), Error> {
             cr.embed(|ce| {
                 ce.title("Server does not exist in config")
                     .description(
-                        "Please add the server to the config using /config_setup if you are the owner of the server.",
+                        "If you are the owner of the server please add the server to the config using /config_setup.",
                     )
                     .color(0x8B0000)
             })
@@ -37,10 +37,16 @@ pub async fn my_infractions(ctx: Context<'_>) -> Result<(), Error> {
 
     match user_infractions {
         Some(user_infractions) => {
+            let user_infractions = if user_infractions == 0 {
+                "no".to_string()
+            } else {
+                user_infractions.to_string()
+            };
+
             ctx.send(|cr| {
                 cr.embed(|ce| {
                     ce.title("Infractions")
-                        .description(format!("You have {} infraction(s).", user_infractions))
+                        .description(format!("You have {} infractions.", user_infractions))
                 })
             })
             .await
